b33b10179975484b18d0d8d7b42cbe112c2595d2
alvaro <alvarocondeorlando@gmail.com>
2022-06-20 18:14:56 -0300

    resgatando usuário por id

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -3,9 +3,17 @@ const router = express.Router();
 
 const UserController = require('../controllers/UserController');
 
+//registro de usuário
 router.post('/register', UserController.register);
+
+//login de usuário
 router.post('/login', UserController.login);
+
+//checando usuário
 router.get('/checkuser', UserController.checkUser);
+
+//resgatando usuário por id
+router.get('/:id', UserController.getUserById);
 
 module.exports = router;
 
